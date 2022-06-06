96d3ef21d03a6defafeabad4a5e9f907921836d0
Lucas Mezencio <lucasmezencio@outlook.com>
2022-06-06 15:20:42 -0300

    hello world: multiple tests inside one function

--- hello.go
+++ hello.go
@@ -2,10 +2,12 @@ package main
 
 import "fmt"
 
+const helloFunctionPrefix = "Hello, "
+
 func main() {
 	fmt.Println(Hello("Lucas"))
 }
 
 func Hello(s string) string {
-	return "Hello, " + s
+	return helloFunctionPrefix + s
 }
